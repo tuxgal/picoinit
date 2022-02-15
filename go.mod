dbd2b97a5ddb8efcc7f4fde0fc7b20fdc8d322ee
G3 <tuxgal@Baymax.area51>
2022-02-14 20:29:38 -0800

    Monitor signals of interest while initializing picoinit.

--- go.mod
+++ go.mod
@@ -4,4 +4,5 @@ go 1.17
 
 require (
 	github.com/tuxdude/zzzlogi v0.0.0-20220214214109-77ae620b508a
+	golang.org/x/sys v0.0.0-20220209214540-3681064d5158
 )
--- service_manager.go
+++ service_manager.go
@@ -3,8 +3,50 @@ package pico
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/tuxdude/zzzlogi"
+	"golang.org/x/sys/unix"
+)
+
+var (
+	// All the signals to monitor.
+	listeningSigs = []os.Signal{
+		unix.SIGHUP,    //  1
+		unix.SIGINT,    //  2
+		unix.SIGQUIT,   //  3
+		unix.SIGILL,    //  4
+		unix.SIGTRAP,   //  5
+		unix.SIGABRT,   //  6
+		unix.SIGIOT,    //  6
+		unix.SIGBUS,    //  7
+		unix.SIGFPE,    //  8
+		unix.SIGKILL,   //  9
+		unix.SIGUSR1,   // 10
+		unix.SIGSEGV,   // 11
+		unix.SIGUSR2,   // 12
+		unix.SIGPIPE,   // 13
+		unix.SIGALRM,   // 14
+		unix.SIGTERM,   // 15
+		unix.SIGSTKFLT, // 16
+		unix.SIGCHLD,   // 17
+		unix.SIGCONT,   // 18
+		unix.SIGSTOP,   // 19
+		unix.SIGTSTP,   // 20
+		unix.SIGTTIN,   // 21
+		unix.SIGTTOU,   // 22
+		// unix.SIGURG,    // 23
+		unix.SIGXCPU,   // 24
+		unix.SIGXFSZ,   // 25
+		unix.SIGVTALRM, // 26
+		unix.SIGPROF,   // 27
+		unix.SIGWINCH,  // 28
+		unix.SIGIO,     // 29
+		unix.SIGPWR,    // 30
+		unix.SIGSYS,    // 31
+	}
 )
 
 // InitServiceManager manages init responsibility in a system (by reaping
@@ -31,15 +73,23 @@ type ServiceInfo struct {
 }
 
 type serviceManagerImpl struct {
-	log zzzlogi.Logger
+	log   zzzlogi.Logger
+	sigCh chan os.Signal
 }
 
 // NewServiceManager instantiates an InitServiceManager along with
 // performing the necessary initialization.
 func NewServiceManager(log zzzlogi.Logger) (InitServiceManager, error) {
 	sm := &serviceManagerImpl{
-		log: log,
+		log:   log,
+		sigCh: make(chan os.Signal, 10),
 	}
+	go sm.signalHandler()
+
+	// Sleep for a short duration to allow the goroutines to initialize.
+	// TODO: Replace this with a channel preferably.
+	time.Sleep(100 * time.Millisecond)
+
 	return sm, nil
 }
 
@@ -49,4 +99,34 @@ func (s *serviceManagerImpl) LaunchServices(services ...*ServiceInfo) error {
 
 func (s *serviceManagerImpl) Wait() int {
 	return -1
+}
+
+func (s *serviceManagerImpl) reapZombies() {
+	s.log.Debugf("Zombie Reaper - Received signal: %q", unix.SIGCHLD)
+
+	// TODO: Implement this.
+}
+
+func (s *serviceManagerImpl) signalHandler() {
+	signal.Notify(s.sigCh, listeningSigs...)
+	for {
+		sig, ok := <-s.sigCh
+		if !ok {
+			s.log.Infof("Signal handler is exiting ...")
+			return
+		}
+
+		if sig == unix.SIGCHLD {
+			s.reapZombies()
+		} else {
+			go s.multicastSig(sig)
+		}
+	}
+}
+
+func (s *serviceManagerImpl) multicastSig(sig os.Signal) int {
+	s.log.Infof("Signal Forwader - Chaining signal: %q to all services", sig)
+
+	// TODO: Implement this.
+	return 0
 }
