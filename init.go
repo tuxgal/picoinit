57593f32e36218d5ae1491c211f67b049dddcf41
G3 <tuxgal@Baymax.area51>
2022-02-20 16:20:32 -0800

    refactor: Smaller modular functions in init.go.

--- init.go
+++ init.go
@@ -13,10 +13,10 @@ type initImpl struct {
 	log zzzlogi.Logger
 	// State machine.
 	state *stateMachine
-	// Service janitor.
-	janitor *serviceJanitor
 	// Signal manager.
 	signals *signalManager
+	// Service/Hook janitor.
+	janitor *serviceJanitor
 }
 
 // NewInit instantiates a Pico Init/Service Manager combo, performs the
@@ -27,45 +27,23 @@ type initImpl struct {
 // on all services to exit. Instead use Init.Wait() to block on the
 // termination of all the services.
 func NewInit(config *InitConfig) (Init, error) {
-	multiServiceMode := len(config.Services) > 1
 	init := &initImpl{
-		log:   config.Log,
-		state: newStateMachine(config.Log),
+		log:     config.Log,
+		state:   newStateMachine(config.Log),
+		signals: newSignalManager(config.Log),
 	}
-
 	init.state.set(stateInitializing)
-	init.signals = newSignalManager(config.Log)
 
-	if config.PreLaunch != nil {
-		init.state.set(stateLaunchingPreHook)
-
-		// Build a new repo and janitor just for managing the pre-hook.
-		var repo *serviceRepo
-		init.janitor, repo = buildJanitor(config.Log, init.signals, false)
-
-		err := launchHook(config.Log, repo, config.PreLaunch)
-		if err != nil {
-			init.shutDown()
-			return nil, fmt.Errorf("failed to launch pre-hook, reason: %v", err)
-		}
-
-		init.janitor.wait()
-		shutDownJanitor(init.signals, init.janitor)
-	}
-
-	// Build the repo and janitor that will be used for managing
-	// the services to be launched.
-	var repo *serviceRepo
-	init.janitor, repo = buildJanitor(config.Log, init.signals, multiServiceMode)
-
-	init.state.set(stateLaunchingServices)
-	err := launchServices(config.Log, repo, config.Services...)
+	err := init.launchPreHook(config.PreLaunch)
 	if err != nil {
-		init.shutDown()
-		return nil, fmt.Errorf("failed to launch services, reason: %v", err)
+		return nil, err
 	}
 
-	init.state.set(stateRunning)
+	err = init.launchServices(config.Services...)
+	if err != nil {
+		return nil, err
+	}
+
 	return init, nil
 }
 
@@ -84,20 +62,54 @@ func (i *initImpl) Wait() int {
 	return exitStatus
 }
 
-func buildJanitor(log zzzlogi.Logger, signals *signalManager, multiServiceMode bool) (*serviceJanitor, *serviceRepo) {
-	signals.clearReapObserver()
-	signals.clearRepo()
-	repo := newServiceRepo(log)
-	janitor := newServiceJanitor(log, repo, multiServiceMode)
-	signals.setRepo(repo)
-	signals.setReapObserver(janitor.handleProcTerminaton)
-	return janitor, repo
+// launchPreHook launches the specified pre-hook and waits till it
+// terminates.
+func (i *initImpl) launchPreHook(hook *Hook) error {
+	if hook == nil {
+		return nil
+	}
+
+	i.state.set(stateLaunchingPreHook)
+
+	// Build a new repo and janitor just for managing the pre-hook.
+	janitor, repo := buildJanitor(i.log, i.signals, false)
+	i.janitor = janitor
+
+	err := launchHook(i.log, repo, hook)
+	if err != nil {
+		i.shutDown()
+		return fmt.Errorf("failed to launch pre-hook, reason: %v", err)
+	}
+
+	i.janitor.wait()
+	shutDownJanitor(i.signals, i.janitor)
+	return nil
 }
 
-func shutDownJanitor(signals *signalManager, janitor *serviceJanitor) {
-	signals.clearReapObserver()
-	janitor.shutDown(signals)
-	signals.clearRepo()
+// launchServices launches the specified list of services.
+func (i *initImpl) launchServices(services ...*Service) error {
+	if len(services) == 0 {
+		i.log.Warnf("Empty list of services specified")
+		// TODO: Avoid building the janitor in this case and just
+		// handle this case gracefully during wait and shut down.
+	}
+
+	i.state.set(stateLaunchingServices)
+
+	// Build the repo and janitor that will be used for managing
+	// the services to be launched.
+	multiServiceMode := len(services) > 1
+	janitor, repo := buildJanitor(i.log, i.signals, multiServiceMode)
+	i.janitor = janitor
+
+	err := launchServices(i.log, repo, services...)
+	if err != nil {
+		i.shutDown()
+		return fmt.Errorf("failed to launch services, reason: %v", err)
+	}
+
+	i.state.set(stateRunning)
+	return nil
 }
 
 // shutDown terminates any running services launched by Init, unregisters
@@ -111,4 +123,23 @@ func (i *initImpl) shutDown() {
 
 	i.state.set(stateHalted)
 	i.log.Infof("All services have terminated!")
+}
+
+// buildJanitor builds a janitor and a repository, and associates them with
+// the specified signal manager.
+func buildJanitor(log zzzlogi.Logger, signals *signalManager, multiServiceMode bool) (*serviceJanitor, *serviceRepo) {
+	signals.clearReapObserver()
+	signals.clearRepo()
+	repo := newServiceRepo(log)
+	janitor := newServiceJanitor(log, repo, multiServiceMode)
+	signals.setRepo(repo)
+	signals.setReapObserver(janitor.handleProcTerminaton)
+	return janitor, repo
+}
+
+// shutDownJanitor shuts down and cleans up the specified janitor.
+func shutDownJanitor(signals *signalManager, janitor *serviceJanitor) {
+	signals.clearReapObserver()
+	janitor.shutDown(signals)
+	signals.clearRepo()
 }
